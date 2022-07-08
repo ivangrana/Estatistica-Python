22729e9ff7020498bdb9e3c39b266f149485542e
Ivan Grana <89265428+ivangrana@users.noreply.github.com>
2022-07-08 15:40:10 -0300

    Create Brownian.py

---
+++ Brownian.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Autor: Ivan Grana
+
+np.random.seed(4)
+n = 10**5 #quantidade de iteracoes
+
+SQN = 1/np.math.sqrt(n) # Formula de atualização
+
+Z_valores = np.random.randn(n)
+Yk = 0
+SB_movimento=list()
+
+for k in range(n):
+     Yk = Yk + SQN*Z_valores[k] #Função de atualização da posição da particula
+     SB_movimento.append(Yk)
+  
+
+plt.plot(SB_movimento)
+plt.title("Movimento Browniano")
+plt.ylabel("Posição Y")
+plt.xlabel("Iterações (n)")
+plt.show()
